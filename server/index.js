c785617f12ed4336a85e2598378bba54bf51c819
makarovas <makarofalex@gmail.com>
2021-04-26 12:57:36 +0300

    init

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema");
+
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+  })
+);
 
 app.listen(5000, () => {
   console.log("server started");
 });
-app.use();
+// app.use();
---
+++ server/schema.js
@@ -0,0 +1,21 @@
+const { buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+
+    type User {
+        id: ID
+        username: String
+        age: Int
+        posts: [Post]
+    }
+
+    type Post {
+        id: ID
+        title: String
+        content: String
+    }
+
+
+`);
+
+module.exports = schema;
